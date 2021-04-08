cead7d25ad69801e3873405f0f92e2d1fdaa0fb9
bcrennen <bchiyeung1996@gmail.com>
2021-04-07 23:45:22 -0700

    finished user login page

---
+++ app/src/main/java/ca/bcit/asahiryoko/UserLoginActivity.java
@@ -0,0 +1,117 @@
+package ca.bcit.asahiryoko;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    private EditText userEmail;
+    private EditText userPassword;
+    private Button loginButton;
+    private Button registerButton;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+
+        userEmail = findViewById(R.id.UserEmail);
+        userPassword = findViewById(R.id.UserPassword);
+        loginButton = findViewById(R.id.LoginBtn);
+        registerButton = findViewById(R.id.RegisterBtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if (firebaseAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        // Register button add listener
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailRegister = userEmail.getText().toString().trim();
+                String passwordRegister = userPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(emailRegister)) {
+                    userEmail.setError("Email is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(passwordRegister)) {
+                    userPassword.setError("Password is required");
+                    return;
+                }
+                if (passwordRegister.length() < 8) {
+                    userPassword.setError("Password must be bigger than 8 characters long.");
+                    return;
+                }
+
+                // Register user
+                firebaseAuth.createUserWithEmailAndPassword(emailRegister, passwordRegister).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(UserLoginActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(UserLoginActivity.this, "ERROR: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String loginEmail = userEmail.getText().toString().trim();
+                String loginPassword = userPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(loginEmail)) {
+                    userEmail.setError("Email required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(loginPassword)) {
+                    userPassword.setError("Password required");
+                    return;
+                }
+
+                if (loginPassword.length() < 8) {
+                    userPassword.setError("Password must be bigger than 8 characters long");
+                    return;
+                }
+
+                // authenticate user
+                firebaseAuth.signInWithEmailAndPassword(loginEmail, loginPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(UserLoginActivity.this, "Login successful!", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(UserLoginActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
