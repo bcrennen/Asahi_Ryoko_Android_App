4f8d5e8e68ad979aee298ee234e663d66a170ad5
Kevin <klee.msn@live.com>
2021-04-09 05:25:34 -0700

    Added Cloud Firestore and Cloud Storage. Added feature where user data is created and stored when user is created.

--- app/src/main/java/ca/bcit/asahiryoko/UserLoginActivity.java
+++ app/src/main/java/ca/bcit/asahiryoko/UserLoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,18 +14,38 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserLoginActivity extends AppCompatActivity {
 
+    /** Used in logging messages and debugging */
+    private static final String TAG = "UserLoginActivity";
     private EditText userEmail;
     private EditText userPassword;
     private Button loginButton;
     private Button registerButton;
 
     FirebaseAuth firebaseAuth;
+
+    /**
+     * Get an instance of the Cloud Firestore.
+     */
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // Create a Cloud Storage reference from the app
+    StorageReference storageRef = FirebaseStorage.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +59,15 @@ public class UserLoginActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        if (firebaseAuth.getCurrentUser() != null) {
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            finish();
-        }
+
+
+//        if (firebaseAuth.getCurrentUser() != null) {
+//            //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//
+//            // FIXME: For testing, remove this line later.
+//            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+//            finish();
+//        }
 
         // Register button add listener
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -69,7 +95,14 @@ public class UserLoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(UserLoginActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                            // FIXME: For testing, remove this line later.
+                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+
+                            //TODO: Add user to database here
+                            create_profile();
+
                         } else {
                             Toast.makeText(UserLoginActivity.this, "ERROR: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                         }
@@ -105,7 +138,10 @@ public class UserLoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(UserLoginActivity.this, "Login successful!", Toast.LENGTH_LONG).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+                            // FIXME: For testing, remove this line later.
+                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         } else {
                             Toast.makeText(UserLoginActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                         }
@@ -113,5 +149,41 @@ public class UserLoginActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    /***
+     * This method creates a user profile when the user creates a new account.
+     * Stores the profile in the firebase database.
+     * Author: Kevin Lee
+     * Date: April 09, 2021
+     * Version: 1.0
+     */
+    public void create_profile() {
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        StorageReference imageRef = storageRef.child("default_profile.jpg");
+        String username = currentUser.getEmail();
+        username = username.split("@")[0];
+
+        // Create a new user with a username, profile picture and bio
+        Map<String, Object> user = new HashMap<>();
+        user.put("username", username);
+        user.put("profilePicture", imageRef.getPath());
+        user.put("bio", "No current bio");
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 }
